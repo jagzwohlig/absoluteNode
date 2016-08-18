7d60ff2f6decc3e3d7861bd8052eef1798096a39
Chintan <chintan@wohlig.com>
2016-08-18 12:41:51 +0530

    changes

--- api/controllers/BankController.js
+++ api/controllers/BankController.js
@@ -5,9 +5,5 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {
-    check: function(req, res) {
-        Office.check(res.callback);
-    }
-};
+var controller = {};
 module.exports = _.assign(module.exports, controller);
