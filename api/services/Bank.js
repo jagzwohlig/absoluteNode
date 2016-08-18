9c9a5dc7d64c77ca8fcc076e7c072a44899ecfb5
Chintan <chintan@wohlig.com>
2016-08-18 13:21:46 +0530

    ok all is well

--- api/services/Bank.js
+++ api/services/Bank.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
+var deepPopulate = require('mongoose-deep-populate')(mongoose);
 var uniqueValidator = require('mongoose-unique-validator');
 var timestamps = require('mongoose-timestamp');
+var validators = require('mongoose-validators');
+var monguurl = require('monguurl');
+require('mongoose-middleware').initialize(mongoose);
+
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
@@ -16,87 +21,13 @@ var schema = new Schema({
     },
 });
 
+schema.plugin(deepPopulate, {});
 schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
 module.exports = mongoose.model('Bank', schema);
 
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
 
-var models = {
-    saveData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        if (data._id) {
-            Model.findOneAndUpdate({
-                _id: data._id
-            }, data, callback);
-        } else {
-            Const.save(callback);
-        }
+var model = {};
 
-    },
-    deleteData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        Config.checkRestrictedDelete(Model, schema, {
-            _id: data._id
-        }, function(err, value) {
-            if (err) {
-                callback(err, null);
-            } else if (value) {
-                console.log(value);
-                Model.findOne({
-                    _id: data._id
-                }).exec(function(err, data2) {
-                    if (err) {
-                        callback("Error Occured", null);
-                    } else if (data2) {
-                        data2.remove({}, callback);
-                    }
-                });
-            } else if (!value) {
-                callback("Can not delete the Object as Restricted Deleted Points are available.", null);
-            }
-        });
-    },
-    getOne: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        Model.findOne({
-            _id: data._id
-        }).exec(callback);
-    },
-    search: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        var maxRow = Config.maxRow;
-
-        var page = 1;
-        if (data.page) {
-            page = data.page;
-        }
-        var field = data.field;
-
-
-        var options = {
-            field: data.field,
-            filters: {
-                keyword: {
-                    fields: ['name'],
-                    term: data.keyword
-                }
-            },
-            sort: {
-                asc: 'name'
-            },
-            start: (page - 1) * maxRow,
-            count: maxRow
-        };
-
-        var Search = Model.find(data.filter)
-            .keyword(options)
-            .order(options)
-            .page(options, callback);
-
-    }
-};
-module.exports = _.assign(module.exports, models);
+module.exports = _.assign(module.exports, exports, model);
--- api/services/Product.js
+++ api/services/Product.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 var deepPopulate = require('mongoose-deep-populate')(mongoose);
 var uniqueValidator = require('mongoose-unique-validator');
 var timestamps = require('mongoose-timestamp');
+var validators = require('mongoose-validators');
+var monguurl = require('monguurl');
+require('mongoose-middleware').initialize(mongoose);
+
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
@@ -37,134 +41,9 @@ schema.plugin(deepPopulate, {
 });
 module.exports = mongoose.model('Product', schema);
 
-var models = {
-    saveData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        if (data._id) {
-            Model.findOne({
-                _id: data._id
-            }, function(err, data2) {
-                if (err) {
-                    callback(err, data2);
-                } else if (data2) {
-                    if (data.category != data2.category) {
-                        Config.manageArrayObject(Category, data2.category, data._id, "product", "delete", function(err, md) {
-                            if (err) {
-                                callback(err, md);
-                            } else {
-                                Config.manageArrayObject(Category, data.category, data._id, "product", "create", function(err, md) {
-                                    if (err) {
-                                        callback(err, md);
-                                    } else {
-                                        data2.update(data, {
-                                            w: 1
-                                        }, callback);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                    else {
-                      data2.update(data, {
-                          w: 1
-                      }, callback);
-                    }
-                } else {
-                    callback("No Data Found", data2);
-                }
-            });
-        } else {
 
-            Const.save(function(err, data2) {
-                if (err) {
-                    callback(err, data2);
-                } else {
-                    Config.manageArrayObject(Category, data2.category, data2._id, "product", "create", function(err, md) {
-                        callback(err, data2);
-                    });
-                }
-            });
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "category.industry", "category.industry"));
 
-        }
+var model = {};
 
-    },
-    deleteData: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        Config.checkRestrictedDelete(Model, schema, {
-            _id: data._id
-        }, function(err, value) {
-            if (err) {
-                callback(err, null);
-            } else if (value) {
-                Model.findOne({
-                    _id: data._id
-                }).exec(function(err, data2) {
-                    if (err) {
-                        callback("Error Occured", null);
-                    } else if (data2) {
-                        Config.manageArrayObject(Category, data2.category, data2._id, "product", "delete", function(err, md) {
-                            if (err) {
-                                callback(err, md);
-                            } else {
-                                data2.remove({}, function(err, data3) {
-                                    if (err) {
-                                        callback(err, data3);
-                                    } else {
-                                        callback(err, data3);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            } else if (!value) {
-                callback("Can not delete the Object as Restricted Deleted Points are available.", null);
-            }
-        });
-    },
-    getOne: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        Model.findOne({
-            _id: data._id
-        }).deepPopulate('category.industry').exec(callback);
-    },
-    search: function(data, callback) {
-        var Model = this;
-        var Const = this(data);
-        var maxRow = Config.maxRow;
-
-        var page = 1;
-        if (data.page) {
-            page = data.page;
-        }
-        var field = data.field;
-
-
-        var options = {
-            field: data.field,
-            filters: {
-                keyword: {
-                    fields: ['name'],
-                    term: data.keyword
-                }
-            },
-            sort: {
-                asc: 'name'
-            },
-            start: (page - 1) * maxRow,
-            count: maxRow
-        };
-
-        var Search = Model.find(data.filter)
-            .keyword(options)
-            .order(options)
-            .deepPopulate('category.industry')
-            .page(options, callback);
-
-    }
-
-};
-module.exports = _.assign(module.exports, models);
+module.exports = _.assign(module.exports, exports, model);
