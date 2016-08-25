e2f9fc08541e0565d22982739d43f549a51e7c17
Chintan <chintan@wohlig.com>
2016-08-25 12:23:36 +0530

    ok man

--- api/controllers/WebController.js
+++ api/controllers/WebController.js
@@ -3,10 +3,5 @@ module.exports = {
         res.callback(null, {
             title: "This is FrontEnd"
         });
-    },
-    index2: function(req, res) {
-        res.callback(null, {
-            Demo: "This is Generated"
-        });
     }
 };
