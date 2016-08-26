1bad5d8cc3dccdf0730a9cdad5c9adf700d87918
Chintan <chintan@wohlig.com>
2016-08-26 18:37:08 +0530

    chnages

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -2,15 +2,14 @@ module.exports = function(grunt) {
 
     grunt.config.set('sass', {
         options: {
-            sourceMap: true,
-            output:"expanded"
+            sourceMap: true
         },
         dist: {
             files: {
-                "frontend/main.css": "frontend/sass/main.scss"
+                "frontend/sass/main.css": "frontend/sass/main.scss"
             }
         }
     });
-    console.log("Demo");
+
     grunt.loadNpmTasks('grunt-sass');
 };
