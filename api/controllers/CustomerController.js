b598be285dd8e2ce8f8439ee3783c67c60d8984d
Chintan <chintan@wohlig.com>
2016-09-07 17:22:07 +0530

    ok

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,16 +6,27 @@
  */
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-  getSegmented: function(req, res) {
-      if (req.body) {
-          req.model.getSegmented(req.body, res.callback);
+    getOfficer: function(data, callback) {
+        if (req.body) {
+            req.model.getOfficer(req.body, res.callback);
 
-      } else {
-          res.json({
-              value: false,
-              data: "Invalid Request"
-          });
-      }
-  },
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+    getSegmented: function(req, res) {
+        if (req.body) {
+            req.model.getSegmented(req.body, res.callback);
+
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
 };
 module.exports = _.assign(module.exports, controller);
