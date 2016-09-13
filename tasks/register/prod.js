ccb6deb0133b3ba4679a7e61ce1ee5589af6750c
Chintan <chintan@wohlig.com>
2016-09-13 20:17:17 +0530

    ok

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -12,5 +12,6 @@
  *
  */
 module.exports = function(grunt) {
-    grunt.registerTask('prod', ['clean:development', 'copy:production', 'sass:production', 'concat:production', 'htmlmin', 'uglify:production', 'cssmin:production', 'clean:productionFiles']);
+    grunt.registerTask('prod', []);
+    // grunt.registerTask('prod', ['clean:development', 'copy:production', 'sass:production', 'concat:production', 'htmlmin', 'uglify:production', 'cssmin:production', 'clean:productionFiles']);
 };
