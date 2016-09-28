69f4eb1c94cc4c7a5b7383c0233c5247c8c89f85
Chintan <chintan@wohlig.com>
2016-09-28 11:38:29 +0530

    changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,9 +1,29 @@
-/**
- * CountryController
- *
- * @description :: Server-side logic for managing countries
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+    loginFacebook: function (req, res) {
+        passport.authenticate('facebook', {
+            scope: ['public_profile', 'user_friends', 'email'],
+            failureRedirect: '/'
+        }, res.socialLogin)(req, res);
+    },
+    loginGoogle: function (req, res) {
+        if (req.query.returnUrl) {
+            req.session.returnUrl = req.query.returnUrl;
+        } else {
+
+        }
+
+        passport.authenticate('google', {
+            scope: ['openid', 'profile', 'email'],
+            failureRedirect: '/'
+        }, res.socialLogin)(req, res);
+    },
+    profile: function (req, res) {
+        if (req.body && req.body.accessToken) {
+            User.profile(req.body, res.callback);
+        } else {
+            res.callback("Please provide Valid AccessToken", null);
+        }
+    }
+};
 module.exports = _.assign(module.exports, controller);
