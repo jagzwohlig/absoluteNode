b520c3706e529b8013ddab6f7b702315164f8f1f
Chintan <chintan@wohlig.com>
2016-10-04 11:50:30 +0530

    chages

--- config/passport/google.js
+++ config/passport/google.js
@@ -2,8 +2,6 @@ global["GoogleKey"] = "AIzaSyC62zlixVsjaq4zDaL4cefNCubjCgxkte4";
 global["GoogleclientId"] = "312627343761-olnlv3n4ined2cn449q2dec3b2rvhd2d.apps.googleusercontent.com";
 global["GoogleclientSecret"] = "xNXKiKG9mHMq08LBb2EKTKfP";
 
-console.log(global["env"]);
-
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
         clientSecret: GoogleclientSecret,
