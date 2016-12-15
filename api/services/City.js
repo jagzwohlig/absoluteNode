3277a858d85fd85aebb5496a8158661fcfddc3e1
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-12-15 14:11:07 +0530

    City Problem Sorted

--- api/services/City.js
+++ api/services/City.js
@@ -87,114 +87,123 @@ var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "district.stat
 var model = {
 
     populateCityDetails: function (data, callback) {
-
-        var keys = _.split(data.keyword, " ");
-        stringMatch = [];
-        _.each(keys, function (key) {
-            var data = {
-                keyword: key
-            };
-
-            stringMatch.push({
-                "name": {
-                    $regex: data.keyword,
-                    $options: 'i'
-                }
-            });
-
-            stringMatch.push({
-                "districts.name": {
-                    $regex: data.keyword,
-                    $options: 'i'
-                }
-            });
-
-            stringMatch.push({
-                "districts.states.name": {
-                    $regex: data.keyword,
-                    $options: 'i'
-                }
-            });
-
-            stringMatch.push({
-                "districts.states.zones.name": {
-                    $regex: data.keyword,
-                    $options: 'i'
-                }
-            });
-
-            stringMatch.push({
-                "districts.states.zones.countries.name": {
-                    $regex: data.keyword,
-                    $options: 'i'
-                }
-            });
-        });
-
-        City.aggregate([{
-            $lookup: {
-                from: "districts",
-                localField: "district",
-                foreignField: "_id",
-                as: "districts"
-            }
-        }, {
-            $unwind: "$districts"
-        }, {
-            $lookup: {
-                from: "states",
-                localField: "districts.state",
-                foreignField: "_id",
-                as: "districts.states"
-            }
-        }, {
-            $unwind: "$districts.states"
-        }, {
-            $lookup: {
-                from: "zones",
-                localField: "districts.states.zone",
-                foreignField: "_id",
-                as: "districts.states.zones"
-            }
-        }, {
-            $unwind: "$districts.states.zones"
-        }, {
-            $lookup: {
-                from: "countries",
-                localField: "districts.states.zones.country",
-                foreignField: "_id",
-                as: "districts.states.zones.countries"
-            }
-        }, {
-            $unwind: "$districts.states.zones.countries"
-        }, {
-            $match: {
-                $or: stringMatch
-            }
-        }, {
-            $project: {
-                _id: 1,
-                city: "$name",
-                district: "$districts.name",
-                state: "$districts.states.name",
-                zone: "$districts.states.zones.name",
-                country: "$districts.states.zones.countries.name"
-            }
-        }, {
-            $limit: 10
-        }], function (err, data4) {
-            if (err) {
-
-            } else {
-                _.each(data4, function (n) {
-                    n.name = n.city + ", " + n.district + ", " + n.state + ", " + n.zone + ", " + n.country;
+        if (data && data.filter._id) {
+            City.search(data, function (err, data1) {
+                data4 = _.cloneDeep(data1);
+                _.each(data4.results, function (n) {
+                    n.name = n.name + ", " + n.district.name + ", " + n.district.state.name + ", " + n.district.state.zone.name + ", " + n.district.state.zone.country.name;
                 });
-                var obj = {
-                    results: data4
+                callback(err, data4);
+            });
+        } else {
+            var keys = _.split(data.keyword, " ");
+            stringMatch = [];
+            _.each(keys, function (key) {
+                var data = {
+                    keyword: key
                 };
-                callback(null, obj)
-            }
-        })
+
+                stringMatch.push({
+                    "name": {
+                        $regex: data.keyword,
+                        $options: 'i'
+                    }
+                });
+
+                stringMatch.push({
+                    "districts.name": {
+                        $regex: data.keyword,
+                        $options: 'i'
+                    }
+                });
+
+                stringMatch.push({
+                    "districts.states.name": {
+                        $regex: data.keyword,
+                        $options: 'i'
+                    }
+                });
+
+                stringMatch.push({
+                    "districts.states.zones.name": {
+                        $regex: data.keyword,
+                        $options: 'i'
+                    }
+                });
+
+                stringMatch.push({
+                    "districts.states.zones.countries.name": {
+                        $regex: data.keyword,
+                        $options: 'i'
+                    }
+                });
+            });
+
+            City.aggregate([{
+                $lookup: {
+                    from: "districts",
+                    localField: "district",
+                    foreignField: "_id",
+                    as: "districts"
+                }
+            }, {
+                $unwind: "$districts"
+            }, {
+                $lookup: {
+                    from: "states",
+                    localField: "districts.state",
+                    foreignField: "_id",
+                    as: "districts.states"
+                }
+            }, {
+                $unwind: "$districts.states"
+            }, {
+                $lookup: {
+                    from: "zones",
+                    localField: "districts.states.zone",
+                    foreignField: "_id",
+                    as: "districts.states.zones"
+                }
+            }, {
+                $unwind: "$districts.states.zones"
+            }, {
+                $lookup: {
+                    from: "countries",
+                    localField: "districts.states.zones.country",
+                    foreignField: "_id",
+                    as: "districts.states.zones.countries"
+                }
+            }, {
+                $unwind: "$districts.states.zones.countries"
+            }, {
+                $match: {
+                    $or: stringMatch
+                }
+            }, {
+                $project: {
+                    _id: 1,
+                    city: "$name",
+                    district: "$districts.name",
+                    state: "$districts.states.name",
+                    zone: "$districts.states.zones.name",
+                    country: "$districts.states.zones.countries.name"
+                }
+            }, {
+                $limit: 10
+            }], function (err, data4) {
+                if (err) {
+
+                } else {
+                    _.each(data4, function (n) {
+                        n.name = n.city + ", " + n.district + ", " + n.state + ", " + n.zone + ", " + n.country;
+                    });
+                    var obj = {
+                        results: data4
+                    };
+                    callback(null, obj)
+                }
+            })
+        }
     }
 
 };
