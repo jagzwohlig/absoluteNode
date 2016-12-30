25cfa990966358433879dc1a6d85326fdeca1ca5
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-12-30 18:13:21 +0530

    ZoneDone

--- api/services/Zone.js
+++ api/services/Zone.js
@@ -2,7 +2,6 @@ var schema = new Schema({
     name: {
         type: String,
         required: true,
-        unique: true,
         uniqueCaseInsensitive: true,
         capitalizeAll: true,
     },
@@ -21,6 +20,10 @@ var schema = new Schema({
         }],
         index: true,
         restrictedDelete: true
+    },
+    uniqueAddress: {
+        type: String,
+        unique: true
     }
 });
 
@@ -38,6 +41,85 @@ module.exports = mongoose.model('Zone', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "country", "country"));
 
-var model = {};
+var model = {
+    saveData: function (data, callback) {
+        var Const = this(data);
+        var foreignKeys = Config.getForeignKeys(schema);
+        Country.findOne({
+            "_id": data.country
+        }).exec(function (err, data2) {
+            if (err) {
+                callback(err, null)
+            } else {
+                Const.uniqueAddress = data2.name + " " + data.name;
+                if (data._id) {
+                    Zone.findOne({
+                        _id: data._id
+                    }, function (err, data2) {
+                        if (err) {
+                            callback(err, data2);
+                        } else if (data2) {
+                            async.each(foreignKeys, function (n, callback) {
+                                if (data[n.name] != data2[n.name]) {
+                                    Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "delete", function (err, md) {
+                                        if (err) {
+                                            callback(err, md);
+                                        } else {
+                                            Config.manageArrayObject(mongoose.models[n.ref], data[n.name], data2._id, n.key, "create", callback);
+                                        }
+                                    });
+                                } else {
+                                    callback(null, "no found for ");
+                                }
+                            }, function (err) {
+                                Zone.find({
+                                    uniqueAddress: Const.uniqueAddress
+                                }).exec(function (err, data3) {
+                                    if (err) {
+                                        callback(err, null);
+                                    } else {
+                                        if (data3.length == 0) {
+                                            data2.update(data, {
+                                                w: 1
+                                            }, callback);
+                                        } else {
+                                            callback("Already Exist", null);
+                                        }
+                                    }
+                                });
+
+
+                            });
+
+
+
+                        } else {
+                            callback("No Data Found", data2);
+                        }
+                    });
+                } else {
+                    Const.save(function (err, data2) {
+                        if (err) {
+                            callback(err, data2);
+                        } else {
+                            async.each(foreignKeys, function (n, callback) {
+                                Config.manageArrayObject(mongoose.models[n.ref], data2[n.name], data2._id, n.key, "create", function (err, md) {
+                                    callback(err, data2);
+                                });
+                            }, function (err) {
+                                callback(err, data2);
+                            });
+
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+    },
+
+};
 
 module.exports = _.assign(module.exports, exports, model);
