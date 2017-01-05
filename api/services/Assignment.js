ecd70cb0908d7c6350e3619e04a4a0d06f502c41
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-01-05 18:40:08 +0530

    31 jan

--- api/services/Assignment.js
+++ api/services/Assignment.js
@@ -774,7 +774,7 @@ var model = {
                 } else {
                   // Changes for Monthly
                   var dateOfApp = moment(new Date(data2.dateOfAppointment)).add(5, "hours").add(30,"minutes").toDate();
-                  green(dateOfApp);
+                  console.log(data2.dateOfAppointment);
                   var firstDay = new Date(dateOfApp.getFullYear(), dateOfApp.getMonth(), 1);
                   var lastDay = new Date(dateOfApp.getFullYear(), dateOfApp.getMonth() + 1, 0);
                   console.log(firstDay,lastDay);
