79b83efaeef4b5d8a3bc8c1bfa73f112a18e519c
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-01-06 15:24:30 +0530

    email attachment image remaning

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -307,10 +307,9 @@ var controller = {
                 $scope.email.to = getFromHeader("To");
                 $scope.email.cc = getFromHeader("Cc");
                 $scope.email.deliveredTo = getFromHeader("Delivered-To");
-               
                 $scope.email.body = base64url.decode($scope.email.body);
+                console.log("Email attachment.................................",$scope.email.attachment[0].body);
                 Config.generatePdf("pdf/abs-emailer", $scope, res.callback);
-
             }
         });
 
