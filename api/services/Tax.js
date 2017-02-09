1afe8f58eecab14caefd237b7a5e0d90baee6725
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-02-09 11:36:18 +0530

    Invoice Done

--- api/services/Tax.js
+++ api/services/Tax.js
@@ -20,5 +20,44 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('Tax', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+    search: function (data, callback) {
+    var Model = this;
+    var Const = this(data);
+    var maxRow = Config.maxRow;
+    var page = 1;
+    // var name1=subString()
+    if (data.page) {
+      page = data.page;
+    }
+    var field = data.field;
+    var options = {
+      field: data.field,
+      filters: {
+        keyword: {
+          fields: ['name'],
+          term: data.keyword
+        }
+      },
+
+      sort: {
+        asc: "createdAt",
+      },
+      start: (page - 1) * maxRow,
+      count: maxRow
+    };
+    _.each(data.filter, function (n, key) {
+      if (_.isEmpty(n)) {
+        n = undefined;
+      }
+    });
+    var Search = Model.find(data.filter)
+      .order(options)
+      .deepPopulate()
+      .keyword(options)
+
+      .page(options, callback);
+
+  },
+};
 module.exports = _.assign(module.exports, exports, model);
