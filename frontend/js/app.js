4342ad241fd9114cf655465dcea7378f9a28a272
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-02-18 19:06:39 +0530

    DOne

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1555,6 +1555,20 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $lo
     $locationProvider.html5Mode(isproduction);
 
 });
+firstapp.filter('amt', function () {
+    return function (nStr) {
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    };
+});
+
 
 firstapp.filter('toobject', function () {
     return function (input, assignment) {
