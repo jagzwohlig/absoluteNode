13f5e248b66fb26509ec81b59a1f399a91523c0d
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-02-21 19:18:51 +0530

    DOne

--- api/services/Role.js
+++ api/services/Role.js
@@ -1,82 +1,18 @@
-/**
- * Role.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
- var mongoose = require('mongoose');
- var Schema = mongoose.Schema;
+var schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: []
+    }
+});
 
- var schema = new Schema({
-     name: String,
-     menu: String,
-     description:String,
-     user_type:String,
-     privileges:String,
+schema.plugin(deepPopulate, {});
+schema.plugin(uniqueValidator);
+schema.plugin(timestamps);
+module.exports = mongoose.model('Role', schema);
 
- });
-
- module.exports = mongoose.model('Role', schema);
- var models = {
-
-     saveData: function(data, callback) {
-       if (data.password && data.password != "") {
-           data.password = md5(data.password);
-       }
-         var role = this(data);
-         if (data._id) {
-             this.findOneAndUpdate({
-                 _id: data._id
-             }, data, function(err, data2) {
-                 if (err) {
-                     callback(err, null);
-                 } else {
-                     callback(null, data2);
-                 }
-             });
-         } else {
-             role.save(function(err, data2) {
-                 if (err) {
-                     callback(err, null);
-                 } else {
-                     callback(null, data2);
-                 }
-             });
-         }
-
-     },
-     getAll: function(data, callback) {
-         this.find({}, {}, {}).exec(function(err, deleted) {
-             if (err) {
-                 callback(err, null);
-             } else {
-                 callback(null, deleted);
-             }
-         });
-     },
-     deleteData: function(data, callback) {
-         this.findOneAndRemove({
-             _id: data._id
-         }, function(err, deleted) {
-             if (err) {
-                 callback(err, null)
-             } else {
-                 callback(null, deleted)
-             }
-         });
-     },
-     getOne: function(data, callback) {
-         this.findOne({
-             _id: data._id
-         }).exec(function(err, data2) {
-             if (err) {
-                 console.log(err);
-                 callback(err, null)
-             } else {
-                 callback(null, data2);
-             }
-         });
-     },
-
- };
- module.exports = _.assign(module.exports, models);
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
+var model = {};
+module.exports = _.assign(module.exports, exports, model);
--- frontend/js/roles.js
+++ frontend/js/roles.js
@@ -36,7 +36,7 @@ angular.module('rolesController', ['templateservicemod', 'navigationservice', 'a
                 isExist: false
             },
             modifyRequest: {
-                val: true,
+                val: false,
                 isExist: false
             },
             approve: {
@@ -71,12 +71,76 @@ angular.module('rolesController', ['templateservicemod', 'navigationservice', 'a
             o.viewAll.isExist = viewAllBool;
             $scope.formData.roles.push(o);
         }
-
-
-        addRoles("Email", "Inbox", "", "", true);
-        addRoles("Company Setup", "Company", "", "", true, true, true, true);
-        addRoles("Company Setup", "Type of Office", "", "", true, true, true, true);
-        addRoles("Company Setup", "Office", "", "", true, true, true, true);
-        addRoles("Company Setup", "Branch", "", "", true, true, true, true);
+        addRoles("Email", "Inbox", "", "", true, false, false, false, false, false, false, false, true);
+        addRoles("Company Setup", "Company", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Company Setup", "Type of Office", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Company Setup", "Office", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Company Setup", "Branch", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Location", "Zone", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Location", "State", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Location", "District", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Location", "City", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Products", "Industry", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Products", "Category", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Products", "Product", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Finance", "Currency", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Finance", "Banks", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Finance", "Activity Type", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Finance", "Expense", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Finance", "Tax", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Customer", "Segment", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Customer", "Company", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Customer", "Customer", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Employee", "Employee", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Employee", "Function", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Employee", "Grade", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Insurance", "Department", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Insurance", "Policy Type", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Insurance", "Policy Doc", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Insurance", "COL", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Insurance", "NOL", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Insurance", "Salvage", "", "", true, true, true, true, true, true, true, true, true);
+        addRoles("Insurance", "Nature Of Survey Code", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Assignments", "Type Of Claims", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "New", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Edit", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "List", "", true, false, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Document Upload", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Document Download", "", true, false, true, true, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Photographs", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Transfer", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Force close request", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Force close approval", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Reopen Request", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "Assignment", "Reopen Approval", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "New", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "Send for Authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "Authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "Revise", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "Release", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "ILA", "Released", "", true, false, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "New", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Send for authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Revise", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Release", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Regenerate", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Assignments", "LOR", "Status", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Templates", "LOR", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Templates", "ILA", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Templates", "ISR", "", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Reimbursement", "Reimbursement Detail", "Request", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Reimbursement", "Reimbursement Detail", "Approval", "", true, false, true, false, false, false, true, true, true);
+        addRoles("Reimbursement", "Reimbursement Detail", "Disbusement", "", true, true, true, true, false, false, false, false, true);
+        addRoles("Knowledge Base", "Knowledge Base list", "Access", "", true, true, true, false, false, false, false, false, true);
+        addRoles("Knowledge Base", "Knowledge Base list", "Request", "", true, true, true, false, false, false, false, false, true);
+        addRoles("Knowledge Base", "Knowledge Base list", "Approval", "", true, true, true, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "New", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Send for Authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Authorize", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Revise", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Release", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Cancle", "", true, true, false, false, false, false, false, false, true);
+        addRoles("Invoice", "Invoice", "Regenerate", "", true, true, false, false, false, false, false, false, true);
 
     });
