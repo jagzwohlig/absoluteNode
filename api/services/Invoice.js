f8b27b4d976f695537707de2a7562880ba273d51
Priyank Parmar <parmarpriyank94@gmail.com>
2017-03-29 20:57:13 +0530

    emaills

--- api/services/Invoice.js
+++ api/services/Invoice.js
@@ -312,7 +312,7 @@ var model = {
                                             } else {
                                                 emailData.insuredName = "";
                                             }
-                                            if (assignmentData.templateIla) {
+                                            if (assignmentData.templateIla[0]) {
                                                 emailData.ilaAuthDate = assignmentData.templateIla[0].authTimestamp;
                                             }
                                             if (assignmentData.products[0]) {
