25a913fca74cffb2ee6414bc155606534900ce20
Chintan <chintan@wohlig.com>
2017-03-30 18:14:16 +0530

    change for currency

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2670,3 +2670,26 @@ firstapp.directive('alphaNumericVehical', function () {
         }
     };
 });
+firstapp.directive('replace', function () {
+    return {
+        require: 'ngModel',
+        scope: {
+            regex: '@replace',
+            with: '@with'
+        },
+        link: function (scope, element, attrs, model) {
+            model.$parsers.push(function (val) {
+                if (!val) {
+                    return;
+                }
+                var regex = new RegExp(scope.regex);
+                var replaced = val.replace(regex, scope.with);
+                if (replaced !== val) {
+                    model.$setViewValue(replaced);
+                    model.$render();
+                }
+                return replaced;
+            });
+        }
+    };
+});
