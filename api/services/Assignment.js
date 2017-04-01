1a631d5e21fdd956c962d544c986d24e4cff2733
Chintan <chintan@wohlig.com>
2017-04-01 14:36:11 +0530

    ok

--- api/services/Assignment.js
+++ api/services/Assignment.js
@@ -2369,14 +2369,14 @@ var model = {
         };
       }
     }
-    
+
     var shareWith = {};
-    if(data.ownerStatus == "Shared with me"){
-        shareWith ={
-          'shareWith.persons':objectid(data.ownerId)
+    if (data.ownerStatus == "Shared with me") {
+      shareWith = {
+        'shareWith.persons': objectid(data.ownerId)
       }
     }
-    var ownerStatus = Object.assign(timelineStatus, name, owner, insurer, insurerd, department, ownerId, intimatedLoss, city, branch, createdAt,shareWith);
+    var ownerStatus = Object.assign(timelineStatus, name, owner, insurer, insurerd, department, ownerId, intimatedLoss, city, branch, createdAt, shareWith);
     console.log("ownerStatus", ownerStatus);
     var pageStartFrom = (data.pagenumber - 1) * data.pagelimit;
     var allTable = [{
