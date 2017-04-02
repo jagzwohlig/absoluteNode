c8b437560478bf8ed63c17e947b3cd7da48e3e77
Chintan <chintan@wohlig.com>
2017-04-02 18:56:26 +0530

    ok

--- config/globals.js
+++ config/globals.js
@@ -119,10 +119,15 @@ global["log"] = function (data) {
 };
 
 //Mime types
-global["mime"]= require('mime-types')
-
+global["mime"] = require('mime-types');
+console.log("ENV");
+console.log(process.env);
 if (process.env.NODE_ENV && process.env.NODE_ENV === "production") {
+    if (process.env.testing) {
+        global["env"] = require("./env/testing.js");
+    }
     global["env"] = require("./env/production.js");
 } else {
     global["env"] = require("./env/development.js");
 }
+console.log(env);
