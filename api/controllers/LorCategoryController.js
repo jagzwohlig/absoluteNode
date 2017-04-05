e69d58e62dbd8c13175e7be35ae5680b9d839335
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2017-04-05 15:07:10 +0530

    Done

--- api/controllers/LorCategoryController.js
+++ api/controllers/LorCategoryController.js
@@ -5,5 +5,16 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
-var controller = {};
+var controller = {
+        getAccordianData: function (req, res) {
+        if (req.body) {
+            req.model.getAccordianData(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: "Invalid Request"
+            });
+        }
+    },
+};
 module.exports = _.assign(module.exports, controller);
--- api/services/LorCategory.js
+++ api/services/LorCategory.js
@@ -12,5 +12,30 @@ schema.plugin(timestamps);
 module.exports = mongoose.model('LorCategory', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema));
-var model = {};
+var model = {
+    getAccordianData: function (data, callback) {
+        LorCategory.find().lean().exec(function (err, data) {
+            if (err) {
+                callback(err, null);
+            } else {
+                console.log("Data",data);
+                async.each(data, function (category, callback) {
+                    LorMaster.find({
+                        lorCategory: category._id
+                    }).lean().exec(function (err, data2) {
+                        if (err) {
+                            callback(err,data2);
+                        }else{
+                            category.subCategory = data2;
+                            console.log("Data 2 ",data2);
+                            callback(null,data2);
+                        }
+                    });
+                }, function (err) {
+                    callback(err, data);
+                });
+            }
+        });
+    }
+};
 module.exports = _.assign(module.exports, exports, model);
